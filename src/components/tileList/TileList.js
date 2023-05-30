b221df7c0556c85f3e62724cfce6f8d9a691001d
mp-web3 <mp.web3@gmail.com>
2023-05-30 09:04:40 +0200

    TileList and Tiles

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = (contacts) => {
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      {}
+      {tiles.map((tile, i) => (
+        <Tile key={i} name={tile.name} description={tile} />
+      ))}
     </div>
   );
 };
